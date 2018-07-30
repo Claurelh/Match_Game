5d2aa01a59f0ac9e98a0ef4d2827b8894a0dae72
Corinne Hatton <claurelh@gmail.com>
2018-07-30 18:19:32 -0400

    Some Javascript

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -1,16 +1,43 @@
-var MatchGame = {};
+var MatchGame = function{};
 
+var MatchGame.generateCardValues = function() {
+  var Numbers = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8","8"];
+  for (var i = 1; i < 9; i++) {
+    order.push(i);
+    for (var c = 1; c < 2; c++) {
+      order.push(i);
+    }
+  }
+  let stop = order.length;
+  console.log(order);
+  var random = [];
+  while (stop > 0) {
+    var index = Math.round(Math.random());
+    random.push(order[index]);
+    order.slice(index,1);
+    stop--;
+  }
+  console.log(random);
+};
 /*
   Sets up a new game after HTML document has loaded.
   Renders a 4x4 board of cards.
 */
 
+// original array
+var Numbers = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8","8"];
+
+(function)(){
+  //var Numbers2 = ["2", "6", "4", "8", "1", "3", "7", "5", "8", "6", "7", "2", "1", "4", "3", "5"];
+
+}
+
+.generateCardValues()
+
 $(document).ready(function() {
-  $(function)(){
-    var Numbers1 = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8","8"];
-    // var nrArray1 = ["2", "6", "4", "8", "1", "3", "7", "5", "8", "6", "7", "2", "1", "4", "3", "5"];
-    var Numbers2 = Numbers1.slice();
-  }
+
+
+
 
 
 
